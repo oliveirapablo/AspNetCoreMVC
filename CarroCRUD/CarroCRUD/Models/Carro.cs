a3419aa0ae7c9bb97c5e7e0851862772ef9fc328
oliveirapablo <pablo.d.o.s96@gmail.com>
2020-03-12 13:07:51 -0300

    first commit

---
+++ CarroCRUD/CarroCRUD/Models/Carro.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarroCRUD.Models
+{
+    public class Carro
+    {
+        private int carroId { get; set; }
+        private string nome { get; set; }
+        
+    }
+}
